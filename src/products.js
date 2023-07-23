8f3b70056b89c39738f8d54bca685f3e8e27b054
Adii05kumar <adi121singh@gmail.com>
2023-07-23 15:31:51 +0530

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,68 @@
+import product1 from './images/11.png';
+import product2 from './images/12.png';
+import product3 from './images/13.png';
+import product4 from './images/14.png';
+import product5 from './images/15.png';
+import product6 from './images/16.png';
+import product7 from './images/17.png';
+import product8 from './images/19.png';
+import product9 from './images/20.png';
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        Price: 79000.00 ,
+        productImage: product1 ,
+    },
+    {
+        id: 2,
+        productName: "Tops",
+        Price: 399.00 ,
+        productImage: product2 ,
+    },
+    {
+        id: 3,
+        productName: "EarBuds",
+        Price: 1100.00 ,
+        productImage: product3 ,
+    },
+    {
+        id: 4,
+        productName: "Sneakers",
+        Price: 8999.00 ,
+        productImage: product4 ,
+    },
+    {
+        id: 5,
+        productName: "Caps",
+        Price: 199.00 ,
+        productImage: product5 ,
+    },
+    {
+        id: 6,
+        productName: "Gauns",
+        Price: 34999.00 ,
+        productImage: product6 ,
+    },
+    {
+        id: 7,
+        productName: "Chinos",
+        Price: 799.00 ,
+        productImage: product7 ,
+    },
+    {
+        id: 8,
+        productName: "Kurti",
+        Price: 999.00 ,
+        productImage: product8 ,
+    },
+    {
+        id: 9,
+        productName: "Watch",
+        Price: 12999.00 ,
+        productImage: product9 ,
+    },
+
+];
